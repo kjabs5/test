32ae09a87273c9d0227cd0ef0a79d950579eafb0
kishor <kishor.jabegu5@gmail.com>
2020-09-02 23:06:54 +1000

    commiteed

---
+++ jpt/src/jpt/Jpt.java
@@ -0,0 +1,14 @@
+package jpt;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Jpt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> book=Arrays.asList("kol","pol","sol");
+        System.out.println("arrays are:"+book);
+	}
+
+}
