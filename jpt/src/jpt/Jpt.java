0d1f4234fbcad88c6e3f15d3fc53f510f3e9c091
kishor <kishor.jabegu5@gmail.com>
2020-09-03 00:22:03 +1000

    aded a for loop

--- jpt/src/jpt/Jpt.java
+++ jpt/src/jpt/Jpt.java
@@ -8,7 +8,11 @@ public class Jpt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<String> book=Arrays.asList("kol","pol","sol");
-        System.out.println("arrays are:"+book);
+		//added a for loop
+		for(String str:book)
+		{
+        System.out.println(str);
+		}
 	}
 
 }
